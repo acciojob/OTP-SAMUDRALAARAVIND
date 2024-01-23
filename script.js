661a2f6ccdc3784376f3cf681232111906076eb3
SAMUDRALA ARAVIND <67514829+SAMUDRALAARAVIND@users.noreply.github.com>
2024-01-23 23:00:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-//your JS code here. If required.
+
+const codeContainer = document.querySelector(".code-container");
+const numberOfInputs = 6;
+let lastEnteredInputElement = null;
+
+function onInput(event) {
+    const currentElement = event.target;
+    console.log("event triggered");
+    if (currentElement.value) {
+        // for doing a click/focus just call the methods click() , and focus() on that HTML element object
+        // console.log("Next element", currentElement.nextElementSibling);
+        // console.log("Previous Element", currentElement.previousElementSibling);
+        lastEnteredInputElement = currentElement;
+        currentElement.nextElementSibling && currentElement.nextElementSibling.focus();
+    }
+}
+
+for (let i = 1; i <= numberOfInputs; i++) {
+    const input = document.createElement("input");
+    input.className = "code";
+    input.maxLength = 1;
+
+    input.addEventListener("input", onInput);
+
+    codeContainer.appendChild(input);
+}
+
+document.addEventListener('keyup', (e) => {
+    console.log(e);
+    if (e.key === "Backspace" && lastEnteredInputElement) {
+        // i need to know last entered input element ?
+        lastEnteredInputElement.value = "";
+        if (lastEnteredInputElement.previousElementSibling) {
+            lastEnteredInputElement.previousElementSibling.focus();
+            lastEnteredInputElement = lastEnteredInputElement.previousElementSibling;
+        }
+        else {
+            lastEnteredInputElement = null;
+        }
+    }
+});
