204de163bd9e009a34e8ea80e572841ff21fa746
SAMUDRALA ARAVIND <67514829+SAMUDRALAARAVIND@users.noreply.github.com>
2024-01-23 23:21:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,27 +15,42 @@ function onInput(event) {
     }
 }
 
+function onBackSpace(e) {
+    // i need to know last entered input element ?
+    // lastEnteredInputElement.value = "";
+    // if (lastEnteredInputElement.previousElementSibling) {
+    //     lastEnteredInputElement.previousElementSibling.focus();
+    //     lastEnteredInputElement = lastEnteredInputElement.previousElementSibling;
+    // }
+    // else {
+    //     lastEnteredInputElement = null;
+    // }
+    e.target.value = "";
+    e.target.previousElementSibling && e.target.previousElementSibling.focus();
+}
+
+function onEnter(e) {
+    if (e.target.value) {
+        e.target.nextElementSibling && e.target.nextElementSibling.focus();
+    }
+}
+
+function onKeyUp(e) {
+    if (e.key === "Enter") {
+        onEnter(e);
+    }
+    else if (e.key === "Backspace") {
+        onBackSpace(e);
+    }
+}
+
 for (let i = 1; i <= numberOfInputs; i++) {
     const input = document.createElement("input");
     input.className = "code";
     input.maxLength = 1;
 
     input.addEventListener("input", onInput);
+    input.addEventListener("keyup", onKeyUp);
 
     codeContainer.appendChild(input);
 }
-
-document.addEventListener('keyup', (e) => {
-    console.log(e);
-    if (e.key === "Backspace" && lastEnteredInputElement) {
-        // i need to know last entered input element ?
-        lastEnteredInputElement.value = "";
-        if (lastEnteredInputElement.previousElementSibling) {
-            lastEnteredInputElement.previousElementSibling.focus();
-            lastEnteredInputElement = lastEnteredInputElement.previousElementSibling;
-        }
-        else {
-            lastEnteredInputElement = null;
-        }
-    }
-});
